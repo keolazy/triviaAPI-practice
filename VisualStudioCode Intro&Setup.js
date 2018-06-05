475ed418a009d116190a95182148bdb79a5f7de9
Nathan Keolasy <keolazy1@gmail.com>
2018-06-05 14:16:32 -0500

    staging all changes for first time

---
+++ VisualStudioCode Intro&Setup.js
@@ -0,0 +1,41 @@
+// Init storage 
+const storage = new Storage();
+
+// Get Stored location data
+const weatherLocation = storage.getLocationData();
+
+// Init weather Object
+const weather = new weather(weatherLocation.city, weatherLocation.state)
+
+// Init UI
+const ui = new UI();
+
+// Get weather on DOM load
+document.addEventListener('DOMContentLaoded', getWeather)
+
+
+// Change Location event
+document.getElementById('w-change-btn').addEventListener('click', (ev) => {
+const city = document.getElementById('city').value
+const state = document.getElementById('state').value
+
+// Change Location
+weather.changeLocation(city, state);
+//Set Location in LS
+storage.setLocationData(city, state);
+
+// Get and display weather
+getWeather()
+
+  // Close modal
+  $('#locModal').modal('hide');
+});
+
+function getWeather() {
+  weather.getWeather()
+    .then(results => {
+      ui.paint(results);
+    })
+    .catch(err => console.log(err));
+}
+
--- app.js
+++ app.js
@@ -1,35 +1,62 @@
 document.addEventListener('DOMContentLoaded', ev => {
-	let resultsArray = null
+	// let resultsArray = null
 	let questionsArray = []
+	let correctPoints = 0
 
-	// const questionList = document.getElementsByClasses('questionBox')[0]
-	// const fetchButton = document.getElementByID('fetchButton')
+	const points = document.getElementsByClassName('pointsCounter')
+	const collectionList = document.getElementsByClassName('collection')[0]
+	const fetchButton = document.getElementById('fetchButton')
+	const submitButton = document.getElementById('submitButton')
 
-	let collectionList = document.getElementById('collection')
+	fetchButton.addEventListener('click', ev => {
+		console.log('fetch Button works')
+		fetchQuestions()
+	})
 
-	fetch(
-		'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple'
-	)
-		.then(res => {
-			return res.json()
-		})
-		.then(myJson => {
-			console.log(myJson)
-			console.log(myJson.results)
-			let resultsArray = myJson.results
-			for (result of resultsArray) {
-				let qa = [result.question, result.correct_answer] // qa stands for question&answer
-				questionsArray.push(qa)
-			}
-			// make into html element here
-			questionsArray.forEach(questionAnswer => {
-				console.log(questionAnswer)
-				let qaHtmlElement = document.createElement('li')
-				qaHtmlElement.setAttribute('class', 'collection-item')
-				collectionList.append(qaHtmlElement)
+	function fetchQuestions() {
+		fetch(
+			'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple'
+		)
+			.then(res => {
+				return res.json()
 			})
-		})
-		.catch(error => {
-			console.error(error)
-		})
+			.then(myJson => {
+				console.log(myJson.results)
+				const resultsArray = myJson.results
+				let buttonIdCount = 0
+				let inputIdCount = 0
+				for (result of resultsArray) {
+					let qa = `<b>Question:</b> ${result.question}  ${
+						result.correct_answer
+					} <input class='answerInput' type='form' id="input${inputIdCount}"></input><p><button class="btn waves-effect waves-light" type="submit" id=button${buttonIdCount}>Submit Answer</button>
+					` // let buttonIdCount++
+
+					// DOM manipulation comes in here.
+					let answerField = document.getElementsByClassName('answerInput')
+					let answerValue = answerField.value
+					// button document.getElementById
+					// button.addEventListener
+
+					let qaHtmlElement = document.createElement('li')
+					qaHtmlElement.setAttribute('class', 'collection-item')
+					qaHtmlElement.innerHTML = qa
+					collectionList.appendChild(qaHtmlElement)
+
+					userResponse(resultsArray)
+				}
+			})
+			.catch(error => {
+				console.error(error)
+			})
+	}
 })
+
+// define variable for input field.value & add this to an eventListener for button;
+function userResponse() {
+	if (this.answerValue == result.correct_answer) {
+		console.log('correct answer! +1 Point for You!!!')
+		correctPoints++
+	} else {
+		console.log('Sorry wrong answer')
+	}
+}
